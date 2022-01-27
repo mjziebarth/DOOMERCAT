2f9c6a871efcdfa0a9468d8a4b97a79bc0c40123
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2022-01-27 10:08:39 +0100

    Create a test case for passing ellipsoid arguments.

---
+++ testing/test_lom_api.py
@@ -0,0 +1,54 @@
+# Test code for using the LabordeObliqueMercator API.
+#
+# Author: Malte J. Ziebarth (ziebarth@gfz-potsdam.de)
+#
+# Copyright (C) 2022 Deutsches GeoForschungsZentrum Potsdam
+#
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+# the European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Licence for the specific language governing permissions and
+# limitations under the Licence.
+
+from doomercat import LabordeObliqueMercator
+import pytest
+
+def test_laborde_oblique_mercator_ellipsoid_signature():
+    """
+    Ensures that the ellipsoid information can be properly deciphered
+    and raises errors if invalid.
+    """
+
+    # Test the predefined ellipsoids:
+    for ellipsoid in ('WGS84','IERS2003','GRS80'):
+        LOM = LabordeObliqueMercator(lonc=20, lat_0=20, alpha=20, k0=1.0,
+                                     ellipsoid='WGS84')
+
+    # Test a custom ellipsoid:
+    LOM = LabordeObliqueMercator(lonc=20, lat_0=20, alpha=20, k0=1.0,
+                                     a=6400.0, f=1.0/300.)
+
+    # Things that should fail:
+    with pytest.raises(RuntimeError):
+        LOM = LabordeObliqueMercator(lonc=20, lat_0=20, alpha=20, k0=1.0,
+                                     ellipsoid='WGS84',
+                                     a=6400.0, f=1.0/300.)
+
+    with pytest.raises(RuntimeError):
+        LOM = LabordeObliqueMercator(lonc=20, lat_0=20, alpha=20, k0=1.0,
+                                     a=6400.0)
+
+    with pytest.raises(RuntimeError):
+        LOM = LabordeObliqueMercator(lonc=20, lat_0=20, alpha=20, k0=1.0,
+                                     f=1.0/300.)
+
+    with pytest.raises(AssertionError):
+        LOM = LabordeObliqueMercator(lonc=20, lat_0=20, alpha=20, k0=1.0,
+                                     ellipsoid='garbage')
