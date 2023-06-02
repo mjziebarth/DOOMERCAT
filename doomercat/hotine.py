Fix elevation scaling II: Rework destination scale factor into elevation factor k_e in Python code.

doomercat/hotine.py
@@ -107,9 +107,7 @@ def _ks(A,B,u,e,phi,lmbd,lmbd0):
     return A*np.cos(B*u/A)*np.sqrt(1-e**2*np.sin(phi)**2)/(np.cos(phi)*np.cos(B*(lmbd-lmbd0)))
 
 
-
-
-def f_d_k_cse(lmbd,phi,a_h_rel,phi0,lmbdc,alphac,k0,e,noJ=False):
+def f_d_k_cse(lmbd,phi,phi0,lmbdc,alphac,k0,e,noJ=False):
 
     if np.abs(alphac) >= np.deg2rad((90-1/3600)):
         alphac = np.deg2rad((90-1/3600))*np.sign(alphac)
@@ -131,9 +129,6 @@ def f_d_k_cse(lmbd,phi,a_h_rel,phi0,lmbdc,alphac,k0,e,noJ=False):
     u = _u(A,S,gamma0,V,B,lmbd,lmbd0)
 
     ks = _ks(A,B,u,e,phi,lmbd,lmbd0)
-
-    # Correct by the local scale:
-    ks /= a_h_rel
 
     if noJ:
         return ks
@@ -220,10 +215,10 @@ def f_d_k_cse(lmbd,phi,a_h_rel,phi0,lmbdc,alphac,k0,e,noJ=False):
         f_dk0 = x24*x30*x44*(A*x55*x7 - x28 - x29)
 
 
-        J = np.stack([f_dphi0/a_h_rel,
-                      f_dlmbdc/a_h_rel,
-                      f_dalphac/a_h_rel,
-                      f_dk0/a_h_rel]).T
+        J = np.stack([f_dphi0,
+                      f_dlmbdc,
+                      f_dalphac,
+                      f_dk0]).T
 
         return (ks,J)
 
@@ -245,28 +240,23 @@ def confine(p):
     return p
 
 
-def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
+def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2, Niter = 100,
          diagnostics=False, k0_ap=None, k0_ap_std=None):
 
     # normalize data weights to sum(wdata) = number of data points
     wdata /= wdata.sum()
 
-    # Compute the local destination scale factor:
+    # Compute the local elevation scale factor k_e:
     e2 = 2*f - f**2
     N = a / np.sqrt(1.0 - e2*np.sin(lat)**2)
-    x = (N+h)*np.cos(lat)
-    z = ((1.0-e2)*N + h) * np.sin(lat)
-    A = z / (x*(1.0-f))
-    r = np.sqrt(x**2 + z**2)
-    re = np.sqrt(1.0 - e2 * A**2 / (1.0 + A**2))
-    a_h_rel = r / (a*re)
+    k_e = np.sqrt(((N*e2 - N + e2*h*np.sin(lat)**2 - h)**2)/(N**2*(e2 - 1)**2))
 
     e = np.sqrt(e2)
 
     p = np.array([phi0,lmbdc,alphac,k0])
 
     x = 2.**np.arange(-1,2.)
-    al = 1e-4
+    al = 1e-3
     la = 1e-1
 
     o = 1.
@@ -277,7 +267,6 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
     vm = 0.
     eps = 1e-10
     ti = 1
-    X = None
     iswitch = None
 
     lminfloat = np.log10(sys.float_info.min)
@@ -313,9 +302,8 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
         v_ap = np.array([0.,0.,0.,k0_ap])
 
     for i in range(Niter):
-
         if pnorm != 0 or not is_p2opt:
-            fk,J = f_d_k_cse(lon,lat,a_h_rel,p[0],p[1],p[2],p[3],e)
+            fk,J = f_d_k_cse(lon, lat, p[0], p[1], p[2], p[3], e)
         else:
             X0 = (_Rz(p[1]) @ _Ry(p[0]) @ _Rx(p[2]-np.pi/2)).T @ X
             Z0 = np.abs(X0[2])
@@ -331,16 +319,16 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
             else:
                 I_batch = np.arange(Z0.size)
 
-            fk = f_d_k_cse(lon[I_batch],lat[I_batch],a_h_rel[I_batch],p[0],p[1],
-                           p[2],p[3],e,noJ = True)
+            fk = f_d_k_cse(lon[I_batch], lat[I_batch], p[0], p[1], p[2], p[3],
+                           e, noJ = True)
 
 
 
         if pnorm == 0. and is_p2opt:
-            res = np.abs(fk-1)
+            res = np.abs(fk - k_e)
             iresmax = np.argmax(res)
-            fk,J = f_d_k_cse(lon[I_batch][iresmax],lat[I_batch][iresmax],
-                             a_h_rel[I_batch][iresmax],p[0],p[1],p[2],p[3],e)
+            fk, J = f_d_k_cse(lon[I_batch][iresmax], lat[I_batch][iresmax],
+                              p[0], p[1], p[2], p[3], e)
             J.shape = (1,4)
 
 
@@ -348,19 +336,18 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
             if is_p2opt:
                 w = np.array([1.])
             else:
-                w = np.abs(fk-1)**(pnorm_p0-2)
+                w = np.abs(fk - k_e)**(pnorm_p0-2)
                 w *= wdata
         elif pnorm > 0. and pnorm <= 1:
-            w = (np.abs(fk-1)+1e-15)**(pnorm-2)
+            w = (np.abs(fk - k_e) + 1e-15)**(pnorm-2)
             w *= wdata
         elif pnorm == 2:
             w = wdata*1.
         else:
-            w = np.abs(fk-1)**(pnorm-2)
+            w = np.abs(fk - k_e)**(pnorm-2)
             w *= wdata
 
         w /= w.sum()
-
 
         if pnorm == 0 and is_p2opt:
 
@@ -373,8 +360,10 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
                 elif cS<-.75:
                     al *= 1+1./20
 
-            Theta =  (np.sign(v_ap[3]-p[3])+1)/2
-            dp = (J*np.sign(fk-1)).flatten() + P_ap @ (p-v_ap) * Theta
+            Theta = (np.sign(v_ap[3] - p[3]) + 1)/2
+            # Here we have:
+            # P_ap[3,3]*(p[3]-v_ap[3]) = P_ap @ (p-v_ap)
+            dp = J*np.sign(fk - k_e).flatten() + P_ap @ (p-v_ap) * Theta
             mm = alm*mm + (1-alm)*dp
             vm = bem*vm + (1-bem)*dp**2
             mt = mm/(1.-alm**ti)
@@ -396,7 +385,8 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
             p = p1
             S33 = np.zeros((1,1))
             Ij,Ik = 0,0
-            S33[0,0] = (np.abs(fk-1)) + P_ap[3,3]*(p[3]-v_ap[3])**2 * Theta
+            Theta = (np.sign(v_ap[3] - p[3]) + 1)/2
+            S33[0,0] = np.abs(fk - k_e) + P_ap[3,3]*(p[3] - v_ap[3])**2 * Theta
 
         else:
 
@@ -415,11 +405,11 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
             neodp = np.zeros((x.size,4))
 
             for j in range(x.size):
-                Theta =  (np.sign(v_ap[3]-p[3])+1)/2
+                Theta = (np.sign(v_ap[3] - p[3]) + 1)/2
                 try:
                    neodp[j] = np.linalg.solve(JTJ @ PJi + x[j] * la * np.eye(4)
                                                  + P_ap * Theta,
-                                              (J.T*w)@(fk-1)
+                                              (J.T*w)@(fk - k_e)
                                                  + P_ap @ (p-v_ap) * Theta)
                 except np.linalg.LinAlgError:
                     error_flag = "LinAlgError"
@@ -429,19 +419,21 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
                 for k in range(x.size):
                     neop[j,k] = confine(p-x[k]*al*neodp[j])
 
-                    neofk = f_d_k_cse(lon,lat,a_h_rel,neop[j,k,0],neop[j,k,1],
-                                      neop[j,k,2],neop[j,k,3],e,noJ=True)
+                    neofk = f_d_k_cse(lon, lat, neop[j,k,0], neop[j,k,1],
+                                      neop[j,k,2], neop[j,k,3], e, noJ=True)
 
+                    Theta = (np.sign(v_ap[3] - neop[j,k,3]) + 1)/2
                     if pnorm == 0:
-                        S33[j,k] = np.sum(wdata*np.abs(neofk-1)**pnorm_p0) \
-                                          + P_ap[3,3]*(p[3]-v_ap[3])**2 * Theta
+                        S33[j,k] = \
+                            np.sum(wdata * np.abs(neofk - k_e)**pnorm_p0) \
+                            + P_ap[3,3] * (neop[j,k,3] - v_ap[3])**2 * Theta
                     else:
-                        S33[j,k] = np.sum(wdata*np.abs(neofk-1)**pnorm) \
-                                          + P_ap[3,3]*(p[3]-v_ap[3])**2 * Theta
+                        S33[j,k] = \
+                           np.sum(wdata * np.abs(neofk - k_e)**pnorm) \
+                           + P_ap[3,3]*(neop[j,k,3] - v_ap[3])**2 * Theta
 
                     if np.isnan(S33[j,k]):
                         S33[j,k] = np.inf
-
 
             I = np.argmin(S33)
             Ij,Ik = np.unravel_index(I,S33.shape)
@@ -474,7 +466,6 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
 
         if i >= Nsd:
             Ssd = np.std(np.log(Sv[-Nsd:]),ddof=1)
-            # print('var',np.log(Sv[-Nsd:]),Ssd)
 
             # Especially for high pnorm, it might happen that the cost evaluation
             # of S33 gives 0.0 for all data points if the distortions are already
@@ -483,6 +474,8 @@ def grad(lon,lat,h,wdata,phi0,lmbdc,alphac,k0,a,f,pnorm=2,Niter = 100,
             #if min(Sv[-Nsd:]) == 0.0:
             if np.isnan(Ssd):
                 Ssd = 0.0
+                error_flag = "CostOutOfDynamicRange"
+                break
 
             if pnorm != 0. and Ssd<Ssd_th:
                 break